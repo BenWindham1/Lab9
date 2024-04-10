ee5ebb45381c4ea6d55b5bbee6ee09df511aa4ec
benmw <benmwind23@gmail.com>
2024-04-10 14:32:36 -0400

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def encode(num):
+    pass
+if __name__ == "__main__":
+    while True:
+        print("Menu\n-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit\n")
+        break
