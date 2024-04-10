065e58766112db110cb4e766280381c421461d9c
benmw <benmwind23@gmail.com>
2024-04-10 14:48:26 -0400

    added encoded function added menu

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 
 
 def encode(num):
-    pass
+    newpass = ""
+    for letter in num:
+        newnum = (int(letter)+3)%10
+        newdig = str(newnum)
+        newpass += newdig
+    return newpass
+
+
+
+
 if __name__ == "__main__":
     while True:
         print("Menu\n-------------")
@@ -9,3 +18,8 @@ if __name__ == "__main__":
         print("2. Decode")
         print("3. Quit\n")
         break
+choice = input("Please Enter an option: ")
+if(choice == "1"):
+    password = input("Please enter your password to encode: ")
+    print(encode(password))
+
